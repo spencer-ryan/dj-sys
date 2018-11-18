8eab4a60f85410ccd5f4f9b5863e65cc710d4530
spencerryan <spencer.ryan100@gmail.com>
2018-11-18 23:34:31 +0000

    Started view profile/user page

---
+++ src/Controller/UserController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+/**
+ * Created by PhpStorm.
+ * User: spencerryan
+ * Date: 18/11/2018
+ * Time: 23:19
+ */
+
+/**
+ * Class UserController
+ * @Route("/user")
+ * @Security("is_granted('IS_AUTHENTICATED_FULLY')")
+ */
+class UserController extends Controller
+{
+    /**
+     * @Route("/view/{user}")
+     * @param User $user
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function view(User $user = null)
+    {
+        if (is_null($user)) {
+            $user = $this->get('security.token_storage')->getToken()->getUser();
+        }
+
+        return $this->render('user/view.html.twig', [
+            'user' => $user,
+        ]);
+    }
+}
